265597d889f231b66d25e2dddf3cf01eb3edc7e4
Raphaël Bellec <contact@bellec.in>
2015-08-18 23:29:27 +0200

    Set default settings after app launch.

--- JSQMessagesSwiftDemo/AppDelegate.swift
+++ JSQMessagesSwiftDemo/AppDelegate.swift
@@ -15,7 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+
+        // Load our default settings
+        NSUserDefaults.saveIncomingAvatarSetting(true)
+        NSUserDefaults.saveOutgoingAvatarSetting(true)
+        
         return true
     }
 
