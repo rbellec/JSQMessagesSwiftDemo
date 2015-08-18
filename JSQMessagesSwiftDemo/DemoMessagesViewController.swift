cd3bc4c20fbe6668b9abef864f8dd2f24858420f
Raphaël Bellec <contact@bellec.in>
2015-08-18 17:49:42 +0200

    Get DemoModelData.swift from ex demo project. Need to add import JSQMessagesViewController

---
+++ JSQMessagesSwiftDemo/DemoMessagesViewController.swift
@@ -0,0 +1,9 @@
+//
+//  DemoMessagesViewController.swift
+//  JSQMessagesSwiftDemo
+//
+//  Created by Raphaël Bellec on 18/08/2015.
+//  Copyright (c) 2015 inventiveWays. All rights reserved.
+//
+
+import Foundation
